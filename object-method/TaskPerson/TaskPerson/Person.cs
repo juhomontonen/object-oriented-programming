fd14518caba3cc6f73d7d87b5aac47f1c1c7c502
juhomontonen <juhop.montonen@gmail.com>
2018-01-11 10:10:13 +0200

    Initial commit to TaskPerson

---
+++ object-method/TaskPerson/TaskPerson/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskPerson
+{
+    class Person
+    {
+        public string Name;
+        private int Age;
+    }
+}
---
+++ object-method/TaskPerson/TaskPerson/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TaskPerson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person personJouni = new Person[];
+            personJouni.Name = "Jouni K";
+            personJouni.
+        }
+    }
+}
