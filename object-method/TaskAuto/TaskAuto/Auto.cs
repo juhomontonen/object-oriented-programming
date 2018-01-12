020274f981475bd321b01d6ee7eca75b17654a65
juhomontonen <juhop.montonen@gmail.com>
2018-01-12 11:02:43 +0200

    Initial commit to TaskAuto

---
+++ object-method/TaskAuto/TaskAuto/Auto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskAuto
+{
+    class Auto
+    {
+        //muuttujat
+        public string Brand;
+        public int Speed;
+
+
+        //konstruktori
+        public Auto(string brand, int speed)
+        {
+            Brand = brand;
+            Speed = speed;
+        }
+
+        public Auto()
+        {
+            Brand = "unknown";
+            Speed = 0;
+        }
+
+        //metodit
+        public void AskData()
+        {
+            Console.Write("Syötä auton merkki: ");
+            Brand = Console.ReadLine();
+
+            Console.Write("Syötä auton nopeus: ");
+            string userInput = Console.ReadLine();
+            Speed = int.Parse(userInput);
+
+
+
+        }
+
+        public void ShowCarInfo()
+        {
+            Console.WriteLine($"Auton merkki on {Brand} ja nopeus {Speed}");
+        }
+    }
+}
---
+++ object-method/TaskAuto/TaskAuto/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TaskAuto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto autoA = new Auto();
+            autoA.AskData();
+
+            autoA.ShowCarInfo();
+            Console.ReadKey();
+        }
+    }
+}
