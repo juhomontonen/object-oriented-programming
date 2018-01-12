21ece2de01b272c2d3e93c051ac9b621658de21d
juhomontonen <juhop.montonen@gmail.com>
2018-01-12 09:30:08 +0200

    Initial commit to TaskKirja.

---
+++ object-method/TaskKirja/TaskKirja/Book.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskKirja
+{
+    class Book
+    {
+        //muuttujat
+        public string Title;
+        public string Author;
+        public int Id;
+        public int Price;
+
+        //konstruktori
+        public Book(string title, string author, int id, int price)
+        {
+            Title = title;
+            Author = author;
+            this.Id = id;
+            Price = price;
+        }
+
+        public Book()
+        {
+            Title = "unknown";
+            Author = "Heikki";
+            Id = 111;
+            Price = 22;
+        }
+
+
+        //metodit
+        public void PrintBookInfo()
+        {
+            Console.WriteLine($"Kirjan nimi: {Title}\nKirjoittaja: {Author}\nId: {this.Id}\nHinta: {this.Price}\n");
+        }
+
+        public string CompareBook(Book book)
+        {
+
+            if (this.Price > book.Price)
+
+                return ($"{this.Title} on kalliimpi kuin {book.Title}");
+            else
+                return ($"{book.Title} on kalliimpi kuin {this.Title}");
+        }
+    }
+}
---
+++ object-method/TaskKirja/TaskKirja/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TaskKirja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book tietoKirja = new Book();
+            tietoKirja.Title = "Tietokirja 1780";
+            tietoKirja.Author = "Matti";
+            tietoKirja.Id = 100;
+            tietoKirja.Price = 22;
+            tietoKirja.PrintBookInfo();
+
+            Book ennatysKirja = new Book();
+            ennatysKirja.Title = "Ennätyskirja";
+            ennatysKirja.Author = "Esa";
+            ennatysKirja.Id = 129;
+            ennatysKirja.Price = 2;
+            ennatysKirja.PrintBookInfo();
+
+            Book autoKirja = new Book("Autokirja 2018","Mauri Kunnas", 1, 20);
+            autoKirja.PrintBookInfo();
+
+            Book keittoKirja = new Book("Keittokirja 2017", "Kaj Kunnas", 2, 36);
+            keittoKirja.PrintBookInfo();
+
+            Console.WriteLine($"{tietoKirja.CompareBook(ennatysKirja)}");
+            Console.WriteLine($"{autoKirja.CompareBook(ennatysKirja)}");
+
+            Console.ReadKey();
+        }
+    }
+}
