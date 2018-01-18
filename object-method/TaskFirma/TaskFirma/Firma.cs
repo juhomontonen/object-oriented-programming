78121ff580d4a724f4e9287bede6aaf7e40f0ce1
juhomontonen <juhop.montonen@gmail.com>
2018-01-18 11:01:14 +0200

    Finished TaskFirma.

---
+++ object-method/TaskFirma/TaskFirma/Firma.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskFirma
+{
+    class Firma
+    {
+        //muuttujat
+        public string Title;
+        public string Address;
+        public string Phone;
+        public int Outcome;
+        public int Expense;
+
+        //konstruktori
+        public Firma(string title, string address, string phone, int outcome, int expense)
+        {
+            Title = title;
+            Address = address;
+            Phone = phone;
+            Outcome = outcome;
+            Expense = expense;
+        }
+
+        public Firma()
+        {
+            Title = "Autokorjaamo Oy";
+            Address = "Koulukatu 1, 53100 Lappeenranta";
+            Phone = "+358 50 538 2889";
+            Outcome = 150;
+            Expense = 100;
+        }
+
+        public Firma(Firma firma)
+        {
+            Title = firma.Title;
+            Address = firma.Address;
+            Phone = firma.Phone;
+            Outcome = firma.Outcome;
+            Expense = firma.Expense;
+        }
+
+        //metodit
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Yrityksen nimi: {Title}\nOsoite: {Address}\nPuhelin: {Phone}\nTulot: {Outcome}\nMenot: {Expense}");
+        }
+
+        public void OutcomePercentage()
+        {
+            double profit = ((double)(Outcome - Expense) / (double)Expense);
+            Console.WriteLine($"Yrityksen {Title} voittoprosentti on: {profit*100}%");
+
+            if (profit <= 1)
+            {
+                Console.WriteLine($"Yrityksellä {Title} menee kehnosti.\n");
+            }
+            else if (profit > 1 && profit < 3)
+            {
+                Console.WriteLine($"Yrityksellä {Title} menee tyydyttävästi.\n");
+            }
+            else if (profit >= 3)
+            {
+                Console.WriteLine($"Yrityksellä {Title} menee hyvin.\n");
+            }
+        }
+
+    }
+}
---
+++ object-method/TaskFirma/TaskFirma/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TaskFirma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Firma newFirma = new Firma("Taksi Oy", "Valtakatu 22", "+358 50 444 4444", 500, 100);
+            newFirma.PrintInfo();
+            newFirma.OutcomePercentage();
+
+            Firma newFirma2 = new Firma();
+            newFirma2.PrintInfo();
+            newFirma2.OutcomePercentage();
+
+            Firma newFirma3 = new Firma(newFirma2);
+            newFirma3.PrintInfo();
+            newFirma3.OutcomePercentage();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
