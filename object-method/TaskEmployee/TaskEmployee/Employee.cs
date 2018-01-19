08139049ab1896a3b8ed40cd3532c62e8d44b1bb
juhomontonen <juhop.montonen@gmail.com>
2018-01-19 09:52:31 +0200

    Added TaskEmployee.

---
+++ object-method/TaskEmployee/TaskEmployee/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskEmployee
+{
+    class Employee
+    {
+        //muuttujat
+        public string Name;
+        public int Id;
+        public string Position;
+        public int Salary;
+
+        //konstruktori
+        public Employee(string name, int id, string position, int salary)
+        {
+            Name = name;
+            Id = id;
+            Position = position;
+            this.Salary = salary;
+        }
+
+        //metodit
+        public void PrintEmployeeInfo()
+        {
+            Console.WriteLine($"Työnantajan nimi: {Name}\nId: {Id}\nTehtävä: {Position}\nPalkka: {Salary}e/h\n");
+        }
+
+        public string CompareSalary(Employee employee)
+        {
+            if (this.Salary > employee.Salary)
+
+                return ($"Työnantajalla {this.Name} on parempi palkka ({this.Salary}e/h) kuin {employee.Name}:lla ({employee.Salary}e/h)");
+            else
+                return ($"Työnantajalla {employee.Name} on parempi palkka ({employee.Salary}e/h) kuin {this.Name}:lla ({this.Salary}e/h)");
+        }
+    }
+}
---
+++ object-method/TaskEmployee/TaskEmployee/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TaskEmployee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employeeA = new Employee("Matti Oy", 1, "Roskakuski", 20);
+            Employee employeeB = new Employee("Esa Ab", 11, "Pitsalähetti", 1);
+            Employee employeeC = new Employee("Heikki Ky", 107, "Opettaja", 34);
+            Employee employeeD = new Employee("Kimmo Ay", 44, "Myyjä", 8);
+
+            Employee[] employees = new Employee[4];
+            employees[0] = employeeA;
+            employees[1] = employeeB;
+            employees[2] = employeeC;
+            employees[3] = employeeD;
+
+            for (int i = 0; i < employees.Length; i++)
+            {
+                employees[i].PrintEmployeeInfo();
+            }
+
+            Console.WriteLine($"{employees[0].CompareSalary(employees[2])}");
+            Console.WriteLine($"{employees[3].CompareSalary(employees[1])}");
+
+            Console.ReadKey();
+        }
+    }
+}
