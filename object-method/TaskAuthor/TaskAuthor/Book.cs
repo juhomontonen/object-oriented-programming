02507af6a6fabe34a0d8275ddf5629b15ebbb89c
juhomontonen <juhop.montonen@gmail.com>
2018-01-25 11:07:18 +0200

    Initial commit to TaskAuthor.

---
+++ object-method/TaskAuthor/TaskAuthor/Book.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskAuthor
+{
+    class Book
+    {
+        //muuttujat
+        public string Title;
+        public string Author;
+        public string Publisher;
+        public double Price;
+        public string Genre;
+        static string ChangeGenre;
+
+        //konstruktori
+        public Book(string title, string author, string publisher, double price, string genre)
+        {
+            Title = title;
+            Author = author;
+            Publisher = publisher;
+            Price = price;
+            Genre = genre;
+        }
+
+        public Book()
+        {
+            Title = " ";
+            Author = " ";
+            Publisher = " ";
+            Price = 0;
+            Genre = " ";
+        }
+
+        //public Book(Book book)
+        //{
+        //    Title = book.Title;
+        //    Author = book.Author;
+        //    Publisher = book.Publisher;
+        //    Price = book.Price;
+        //    Genre = book.Genre;
+        //}
+
+        //metodit
+        public void PrintBookInfo()
+        {
+            Console.WriteLine($"Kirjan nimi: {Title}\nKirjoittaja: {Author}\nKustantaja: {Publisher}\nHinta: {Price}\nGenre: {Genre}\n");
+        }
+
+        public void GetBook(Book book)
+        {
+            Console.Write("Syötä kirjan nimi: ");
+            this.Title = Console.ReadLine();
+
+            if (this.Title.Equals(Title))
+                PrintBookInfo();
+            else
+                Console.WriteLine($"Kirjaa {this.Title} ei löytynyt.");
+        }
+
+        public static void GenreChange()
+        {
+            Console.WriteLine(ChangeGenre);
+        }
+
+    }
+}
---
+++ object-method/TaskAuthor/TaskAuthor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TaskAuthor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book newBook1 = new Book("Jamie Oliverin yrttikirja", "Matti", "Mauri Kunnas Oy", 30, "Keittokirja");
+            Book newBook2 = new Book("Hauskoja juttuja", "Kimmo", "Kustannus Ay", 22, "Vitsikirja");
+
+            newBook1.GetBook(newBook1);
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
