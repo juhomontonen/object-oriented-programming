683de2318e9969244d335dfd016398a0548435e4
juhomontonen <juhop.montonen@gmail.com>
2018-02-08 09:16:01 +0200

    Initial commit to TaskVehicle.

---
+++ object-method/TaskVehicle/TaskVehicle/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskVehicle
+{
+    class Car : Vehicle
+    {
+        //muuttujat
+        protected double MotorSize;
+        protected string Model;
+        protected int Doors;
+
+        //konstruktori
+        public Car(string type, string brand, int year, double price, double motorsize, string model, int doors) : base(type, brand, year, price)
+        {
+            Type = type;
+            Model = model;
+            Year = year;
+        }
+
+        //metodit
+        public override void PrintVehicleInfo()
+        {
+            base.PrintVehicleInfo();
+            Console.WriteLine($"Moottorin koko: {MotorSize}\nMalli: {Model}\nVuosimalli: {Year}\n");
+        }
+
+
+
+    }
+}
---
+++ object-method/TaskVehicle/TaskVehicle/Vehicle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskVehicle
+{
+    class Vehicle
+    {
+        //muuttujat
+        protected string Type;
+        protected string Brand;
+        protected int Year;
+        protected double Price;
+
+        //konstruktori
+        public Vehicle(string type, string brand, int year, double price)
+        {
+            Type = type;
+            Brand = brand;
+            Year = year;
+            Price = price;
+        }
+
+        //metodit
+        virtual public void PrintVehicleInfo()
+        {
+            Console.WriteLine($"Kulkuneuvon tiedot\nTyyppi: {Type}\nMerkki: {Brand}\nVuosimalli: {Year}\nHinta: {Price}\n");
+        }
+
+
+    }
+}
